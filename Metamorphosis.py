0b10836a097e834903ad7ab2cfa8acfdb42fc815
Dylan-Kile <dylankile@utexas.edu>
2017-02-12 19:41:29 -0600

    Update Metamorphosis.py

--- Metamorphosis.py
+++ Metamorphosis.py
@@ -2,17 +2,18 @@ import re
 allwords = []
 file = open("metamorphosis.txt", "r")
 line = file.readline()
-while line != "":
-  if line == "\n":
+while True:
+  if(line == "\n"):
     line = file.readline()
+    continue
+  if(line == ""):
+    break
   line.rstrip()
   line = line.lower()
   words = line.split(" ")
   for string in words:
     string = re.findall('[a-z]+',string)
     allwords.append("".join(str(elt) for elt in string))
-
-  
   line = file.readline()
 
 file.close()
@@ -29,8 +30,24 @@ for elt in allwords:
     word_dict[elt] = 1
     unique_file.write(elt + "\n")
     
-print(word_dict)
+
 w_file.close()
 unique_file.close()
 
+
+frequency_dict = {}
+for word, frequency in word_dict.items():
+    if frequency in frequency_dict:
+        frequency_dict[frequency] += 1
+    else:
+        frequency_dict[frequency] = 1
+        
+   
+f_file = open("wordfrequency.txt", "w")
+for frequency, numberOfWords in sorted(frequency_dict.items()):
+    s =  str(frequency) + ": " + str(numberOfWords) + "\n"
+    
+    f_file.write(s)
+
+f_file.close()
 
